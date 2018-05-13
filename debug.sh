0a995ff03cd523d2c546f13fb1b9f6c40bd34a3b
Javier Ruiz <javier.ruiz@outlook.de>
2018-05-13 08:30:15 +0000

    Fix debug.sh

--- debug.sh
+++ debug.sh
@@ -3,4 +3,4 @@ mkdir -p build-debug
 cd build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../ledify
 make
-sudo ln -f -s $(pwd)/src/ledify_rpi /usr/local/bin/ledify
+sudo ln -f -s $(pwd)/src/ledify /usr/local/bin/ledify
