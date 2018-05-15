c0c5cce8950521bf6e1e78c1e5b19f31b7e8f00b
Javier Ruiz <javier.ruiz@outlook.de>
2018-05-15 18:36:46 +0000

    Feature: added ninja

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p build-release
 cd build-release
-cmake -DCMAKE_BUILD_TYPE=Release ../ledify
-make
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ../ledify
+cmake --build .
 sudo ln -f -s $(pwd)/src/ledify /usr/local/bin/ledify
