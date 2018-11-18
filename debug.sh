d520e87cf1e80790786cfa4d0fae3b865ca6ed43
Javier Ruiz <javier.ruiz@outlook.de>
2018-11-18 17:22:44 +0000

    compile with clang

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p build-debug
 cd build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ../ledify
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -G Ninja ../ledify
 cmake --build .
 sudo ln -f -s $(pwd)/src/ledify /usr/local/bin/ledify
--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p build-release
 cd build-release
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ../ledify
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang ../ledify
 cmake --build .
 sudo ln -f -s $(pwd)/src/ledify /usr/local/bin/ledify
