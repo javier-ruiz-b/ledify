287a3e80f452279153ed8e765f671f585b07f42b
Javier Ruiz <javier.ruiz@outlook.de>
2018-12-15 22:49:03 +0100

    New animation when turning on

--- ledify/src/CommandExecutor.cpp
+++ ledify/src/CommandExecutor.cpp
@@ -126,24 +126,27 @@ void CommandExecutor::cOff(const QStringList &, QString &) {
 }
 
 void CommandExecutor::cOn(const QStringList &, QString &) {
-    QSharedPointer<Layer> red(new SpotLayer(Color(203, 1, 1, 1), 50, 50, Interpolator::InterpolatorLinear));
-    QSharedPointer<Layer> green(new SpotLayer(Color(1, 203, 1, 1), 200, 50, Interpolator::InterpolatorLinear));
-    QSharedPointer<Layer> blue(new SpotLayer(Color(1, 1, 203, 1), 150, 150, Interpolator::InterpolatorLinear));
-    QSharedPointer<Layer> white(new SpotLayer(Color(203, 80, 1, 203), 150, 150, Interpolator::InterpolatorLinear));
+    QSharedPointer<Layer> red(new SpotLayer(Color(200, 0, 0, 0), 150, 25, Interpolator::InterpolatorLinear));
+    QSharedPointer<Layer> green(new SpotLayer(Color(0, 200, 0, 0), 200, 50, Interpolator::InterpolatorLinear));
+    QSharedPointer<Layer> blue(new SpotLayer(Color(0, 0, 200, 0), 150, 50, Interpolator::InterpolatorLinear));
+    QSharedPointer<Layer> white(new SpotLayer(Color(203, 80, 1, 203), 150, 15, Interpolator::InterpolatorLinear));
+    QSharedPointer<Layer> spot(new SpotLayer(Color(203, 80, 1, 203), 150, 150, Interpolator::InterpolatorLinear));
 
     auto redAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(red, 0.15f));
-    auto greenAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(green, -0.15f));
-    auto blueAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(blue, 0.35f));
-    auto whiteAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(blue, -0.35f));
+    auto redAnimation2 = QSharedPointer<Layer> (new SlideAnimationLayer(red, -0.15f));
+    auto greenAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(green, -0.25f));
+    auto blueAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(blue, 0.25f));
+    auto whiteAnimation = QSharedPointer<Layer> (new SlideAnimationLayer(white, -0.1f));
+    auto whiteAnimation2 = QSharedPointer<Layer> (new SlideAnimationLayer(white, 0.1f));
 
-    auto allLayers = QSharedPointer<Layer> (new AdditionLayer({redAnimation, greenAnimation, blueAnimation, whiteAnimation}));
+    auto allLayers = QSharedPointer<Layer> (new AdditionLayer({redAnimation, redAnimation2, greenAnimation, blueAnimation, whiteAnimation, whiteAnimation2}));
 
     auto fadeStart = new FadeLayer(m_layers->current(),
                               allLayers,
                               Interpolator::InterpolatorAccelerate, 0, 1000);
     auto fadeEnd = new FadeLayer(QSharedPointer<Layer> (fadeStart),
-                              white,
-                              Interpolator::InterpolatorAccelerate, 4000, 5500);
+                              spot,
+                              Interpolator::InterpolatorAccelerate, 3000, 4000);
     m_layers->setAsRoot(fadeEnd);
 }
 
