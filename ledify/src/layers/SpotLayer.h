279920835758a69674d1536b5bbe250a73f644ea
Developer <developer@debian>
2019-08-23 18:58:18 +0200

    Fix memory leak

--- ledify/src/layers/SpotLayer.h
+++ ledify/src/layers/SpotLayer.h
@@ -7,10 +7,11 @@ class Color;
 class SpotLayer : public Layer {
 public:
     SpotLayer(const Color &color, float position, float size, Interpolator::Type interpolator);
+    virtual ~SpotLayer() override;
 
     virtual void draw(uint32_t *buffer, uint32_t size) override;
 
- private:
+private:
     uint32_t *m_pixelBuffer = nullptr;
     uint32_t m_bufferSize;
     float m_position;
