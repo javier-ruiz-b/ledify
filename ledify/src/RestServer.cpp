d6671621b93f6d72d82cd856092e5ef3afd4d7a6
Javier Ruiz <javier.ruiz@outlook.de>
2019-10-03 19:11:37 +0200

    Made RestServer non blocking by placing it in another thread

--- ledify/src/RestServer.cpp
+++ ledify/src/RestServer.cpp
@@ -1,17 +1,39 @@
 #include "RestServer.h"
 
 #include <QLoggingCategory>
+#include <QtConcurrent/QtConcurrent>
 #include <cpp-httplib/httplib.h>
 
 Q_LOGGING_CATEGORY(REST, "ledify.restserver", QtWarningMsg)
 
 using namespace httplib;
 
+
 RestServer::RestServer(QObject *parent) : QObject(parent) {
+    m_server = new Server();
+    m_threadPool = new QThreadPool(this);
 
-    Server svr;
+    connect(this, &RestServer::executeCommand, this, &RestServer::commandHandler);
 
-    svr.Get(R"(.*)", [this](const Request& req, Response& res) {
+    m_threadPool->setMaxThreadCount(1);
+    QtConcurrent::run(m_threadPool, [this] {
+        startServer();
+    });
+
+    qCDebug(REST, "Started REST server");
+}
+
+RestServer::~RestServer() {
+    m_server->stop();
+    delete m_server;
+}
+
+QString RestServer::commandHandler(QString command) {
+    return m_callback(command);
+}
+
+void RestServer::startServer() {
+    m_server->Get(R"(.*)", [this](const Request& req, Response& res) {
         QString urlString = req.path.c_str();
 
        if (urlString.size() < 2) {
@@ -24,16 +46,17 @@ RestServer::RestServer(QObject *parent) : QObject(parent) {
        QByteArray responses = "";
        foreach(auto command, commands.split("+", QString::SplitBehavior::SkipEmptyParts)) {
            qCDebug(REST) << "Received command" << command;
-           responses += m_callback(command).toUtf8() + ";";
+           auto response = emit executeCommand(command).toUtf8();
+           responses += response + ";";
        }
        res.set_content(responses.data(), "text/plain");
 
     });
 
-
-    if (svr.listen("0.0.0.0", 8033)) {
-        qCDebug(REST, "Started REST server");
+    if (m_server->listen("0.0.0.0", 8033)) {
+        qCDebug(REST, "Stopped REST server");
     } else {
-        qCWarning(REST, "Failed to start REST server");
+        qCWarning(REST, "Error initializing REST server");
     }
+
 }
--- ledify/src/RestServer.h
+++ ledify/src/RestServer.h
@@ -1,17 +1,34 @@
 #pragma once
 #include <QObject>
 #include <QString>
+#include <QThreadPool>
 #include <functional>
+
+namespace httplib {
+class Server;
+}
 
 class RestServer : public QObject {
     Q_OBJECT
 
 public:
     RestServer(QObject *parent = nullptr);
+    ~RestServer();
+
     void registerCallback(std::function<QString(QString &)> callback) { m_callback = callback; }
+
+signals:
+    QString executeCommand(QString command);
+
+public slots:
+    QString commandHandler(QString command);
 
 
 private:
+    void startServer();
+
     std::function<QString(QString &)> m_callback = [](QString &){return QString(); };
+    httplib::Server *m_server;
+    QThreadPool *m_threadPool;
 
 };
